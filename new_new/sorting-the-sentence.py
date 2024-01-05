461a1c318b19ae52fa4ef279dbf9b433f0e764a8
achillesocean <143838608+achillesocean@users.noreply.github.com>
2024-01-06 01:17:13 +0300

    Added solution for Sorting the Sentence

--- new_new/sorting-the-sentence.py
+++ new_new/sorting-the-sentence.py
@@ -1,11 +1,5 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        a = s.split()
-        b = [0] * len(a)
-        for i in a:
-            b[int(i[-1]) - 1] = i[0:-1]
-        return " ".join(b)
-        
         temp = s.split()
         temp.sort(key=lambda x: x[-1])
         ans = []
