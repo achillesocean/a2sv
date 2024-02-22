c7c4e733f57afaf5f0bc30552c46520a94e88695
achillesocean <143838608+achillesocean@users.noreply.github.com>
2024-02-22 09:06:32 +0300

    Add solution for Reverse String

---
+++ leetcode/reverse-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        n = len(s) - 1
+        #two pointers and recursion
+            #hello
+            #olleh
+        def reverse(left, right):
+            if left > right: return
+            s[left], s[right] = s[right], s[left]
+            reverse(left + 1, right - 1)
+        reverse(0, n)
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
